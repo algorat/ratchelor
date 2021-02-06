bd46f0cd8609886d3ec5d8653cf0d99881ff602e
Connie Ye <constanceye1316@gmail.com>
2021-02-06 18:42:47 -0500

    add class for reactions

---
+++ src/components/ReactionAnimation.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class ReactionAnimation extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="reaction-animation">
+        {
+          Array(5).fill(this.props.emote)
+            .map((val, idx) => 
+              <div className={"reaction-emote " + "reaction-emote-" + (idx + 1)}>{val}
+              </div>)
+        }
+      </div>
+    );
+  }
+}
+
+export default ReactionAnimation;
+
