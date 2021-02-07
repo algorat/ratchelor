c45952ecbab3761b4fc373241d96a6609d99a427
carolinehermans <carooolinehermans@gmail.com>
2021-02-06 20:32:22 -0800

    fade between intro and select

--- src/components/IntroScreen.js
+++ src/components/IntroScreen.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 class IntroScreen extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      opacity: 1,
+    }
+  }
+
+  onClick() {
+    this.fadeInterval = window.setInterval(() => {
+      let opacity = this.state.opacity - 0.02;
+      if (opacity <= 0) {
+        this.props.onClick();
+        window.clearInterval(this.fadeInterval);
+      } else {
+        this.setState({opacity});
+      }
+    }, 5);
   }
 
   render() {
     return (
-      <div id="introScreen" className="screen">
-        <button onClick={this.props.onClick}>Embark</button>
+      <div id="introScreen" className="screen" style={{opacity: this.state.opacity}}>
+        <button onClick={this.onClick.bind(this)}>Embark</button>
         <div id="hideme"/>
     </div>
     );
