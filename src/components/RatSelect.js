3857cc7cff2a7b1b209db5d88c03b4f6ed12e7fc
carolinehermans <carooolinehermans@gmail.com>
2021-02-06 22:57:46 -0800

    new rat select

--- src/components/RatSelect.js
+++ src/components/RatSelect.js
@@ -12,7 +12,7 @@ class RatSelect extends React.Component {
 
     this.state = {
       selectedRats: [],
-      selectRatsButton: ""
+      selectRatsButton: <div id="chooseText">Select your 7 contestants</div>
     }
   }
 
@@ -22,13 +22,19 @@ class RatSelect extends React.Component {
     const element = document.getElementById(id);
 
     // If it's already chosen, deselect it
-    if (this.state.selectedRats.indexOf(ratName) != -1) {
+    if (this.state.selectedRats.indexOf(ratName) !== -1) {
       const index = this.state.selectedRats.indexOf(ratName);
       const newSelectedRats = this.state.selectedRats;
       newSelectedRats.splice(index, 1);
       this.setState({selectedRats: newSelectedRats});
       element.classList.remove("selectedRat");
-      this.setState({selectRatsButton: ""})
+      if (this.state.selectedRats.length !== this.props.numRatsInGame - 1) {
+        let selectRatsButton = <div id="chooseText">{`Select ${this.props.numRatsInGame - this.state.selectedRats.length} more contestants`}</div>
+        this.setState({selectRatsButton});
+      } else {
+        let selectRatsButton = <div id="chooseText">{`Select ${this.props.numRatsInGame - this.state.selectedRats.length} more contestant`}</div>
+        this.setState({selectRatsButton});
+      }
       return;
     }
 
@@ -44,13 +50,18 @@ class RatSelect extends React.Component {
 
     // If that was the final rat, display the advance button
     if (this.state.selectedRats.length === this.props.numRatsInGame) {
-      let selectRatsButton = <button onClick={this.onClickSelectRats}>Continue</button>
+      let selectRatsButton = <div id="chooseText"><button onClick={this.onClickSelectRats}>Continue</button></div>
+      this.setState({selectRatsButton});
+    } else if (this.state.selectedRats.length !== this.props.numRatsInGame - 1) {
+      let selectRatsButton = <div id="chooseText">{`Select ${this.props.numRatsInGame - this.state.selectedRats.length} more contestants`}</div>
       this.setState({selectRatsButton});
     } else {
-      this.setState({selectRatsButton: ""});
+      let selectRatsButton = <div id="chooseText">{`Select ${this.props.numRatsInGame - this.state.selectedRats.length} more contestant`}</div>
+      this.setState({selectRatsButton});
     }
 
     // Force a UI update for the rat list
+
     this.setState({});
   }
 
@@ -59,12 +70,16 @@ class RatSelect extends React.Component {
     // Create a clickable div for every rat in the game
     for (let i = 0; i < this.props.rats.length; i++) {
       let filename = `/ratchelor/img/Frames/${this.props.rats[i].filename}.PNG`
+      let filenameHearts = `/ratchelor/img/Frames/hearts${(i  % 9) + 1}.PNG`
       ratsList.push(
         <div key={i} id="ratContainer">
           <div  id={`rat${i}`} className="ratListItem" onClick={() => {
             this.selectRat(this.props.rats[i].name, `rat${i}`);
           }}>
-          <img className="ratPic" src={filename}/>
+          <div className="ratPic">
+            <img className="ratFrame" src={filename}/>
+            <img className="ratHearts" src={filenameHearts}/>
+          </div>
           <div className="ratNameContainer">
           <div className="ratName">{`${this.props.rats[i].name}`}</div>
           <div className="ratTagline">{`"${this.props.rats[i].tagline}"`}</div>
@@ -76,9 +91,9 @@ class RatSelect extends React.Component {
     
     return (
       <div id="ratSelectScreen" className="screen">
-        <div id="chooseText">Select your {`${this.props.numRatsInGame}`} contestants:</div>
+                {this.state.selectRatsButton}
         <div id="ratListContainer">{ratsList}</div>
-        {this.state.selectRatsButton}
+
     </div>
     );
   }
