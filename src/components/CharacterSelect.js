d6c04046fb3409b2f432e4370e49ab7bd3b52471
carolinehermans <carooolinehermans@gmail.com>
2021-02-09 15:57:28 -0800

    ui updates

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -2,6 +2,8 @@ import React from "react";
 
 class CharacterImg extends React.Component {
 
+  
+
   render() {
     return (
       <div onClick={this.props.onClick} 
@@ -16,35 +18,58 @@ class CharacterImg extends React.Component {
 }
 
 class CharacterSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ratSelected: false,
+      transform: 1
+    }
+  }
+
+  componentDidMount() {
+    // this.interval = window.setInterval(() => {
+    //   let transform = -1 * this.state.transform
+    //   this.setState({transform})
+    //   console.log(transform)
+    // }, 1000);
+  }
+  
 
   render() {
+    let buttonArea;
+    if (this.state.ratSelected) {
+      buttonArea = <div id="button-container">
+      <button 
+        onClick={this.props.onClick}
+        style={{display: this.props.playerIdx >= 0 ? "block" : "none"}}>
+          Onwards!
+      </button>
+    </div>
+    } else {
+      buttonArea =  <div id="ratchelor-intro-question">
+      Which rat do you want to be?
+    </div>
+    }
     return (
       <div id="custom-character" className="screen">
-        <div id="ratchelor-intro">
-          You are the ratchelor, a single rat looking for love. 
-          You're about the enter a dating experience 
-          where you will meet many rats and slowly choose who to keep, 
-          round after round, until you find your true love.
+        <div id="ratchelor-intro-container">
+        <img id="hearts-header"  src="/ratchelor/img/player/playerselecthearts.png"></img>
+        <div id="ratchelor-intro-title">
+          You are The Ratchelor, 
         </div>
-        <div id="ratchelor-intro-question">
-          To begin, which rat are you?
+        <div id="ratchelor-intro"> 
+        A single rat looking for love. You will meet many rats and slowly choose who to keep, round after round, until you find true love.</div>
         </div>
         <div id="custom-character-row">
           {Array(4).fill("").map((_, idx) => 
             <CharacterImg 
               selected={(idx + 1) === this.props.playerIdx} 
-              onClick={() => this.props.changePlayerIdx(idx + 1)}
+              onClick={() => {this.props.changePlayerIdx(idx + 1); this.setState({ratSelected: true});}}
               key={`char${idx}`} src={`/ratchelor/img/Player/${idx + 1}_intro.png`}
             />)
           }
         </div>
-        <div id="button-container">
-          <button 
-            onClick={this.props.onClick}
-            style={{display: this.props.playerIdx >= 0 ? "block" : "none"}}>
-              Onwards!
-          </button>
-        </div>
+        {buttonArea}
       </div>
     );
   }
