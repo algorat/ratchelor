cb8475bd82a9de64c6fe5e9d09c2622022311905
Connie Ye <constanceye1316@gmail.com>
2021-02-11 13:42:09 -0500

    built demo & added reactions

--- src/components/ReactionAnimation.js
+++ src/components/ReactionAnimation.js
@@ -7,11 +7,12 @@ class ReactionAnimation extends React.Component {
 
   render() {
     return (
-      <div className="reaction-animation">
+      <div className="reaction-animation" style={{left: `${this.props.left}%`, top: this.props.top + "%"}}>
         {Array(5)
           .fill(this.props.emote)
           .map((val, idx) => (
-            <div className={"reaction-emote " + "reaction-emote-" + (idx + 1)}>
+            <div key={"reaction-emote-" + (idx + 1)} 
+            className={"reaction-emote " + "reaction-emote-" + (idx + 1)}>
               {val}
             </div>
           ))}
