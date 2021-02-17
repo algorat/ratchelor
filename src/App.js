d1d10f20d97ad08926643e4eaff1ae7347f80edf
Connie Ye <constanceye1316@gmail.com>
2021-02-17 02:30:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ var firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
   measurementId: process.env.REACT_APP_MEASUREID
 };
 
