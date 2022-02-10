58158a072bac532312f2eab8ad9694a08eb520db
Tatyana <tmustako@gmail.com>
2022-02-09 23:30:27 -0500

    json update start and image adding

--- src/components/RatSelect.js
+++ src/components/RatSelect.js
@@ -99,7 +99,7 @@ class RatSelect extends React.Component {
     let ratsList = [];
     // Create a clickable div for every rat in the game
     for (let i = 0; i < this.props.rats.length; i++) {
-      let filename = `/ratchelor/img/Frames/${this.props.rats[i].filename}.PNG`;
+      let filename = `/ratchelor/img/Frames/${this.props.rats[i].filename}-frame.PNG`;
       let filenameHearts = `/ratchelor/img/Frames/hearts${(i % 9) + 1}.PNG`;
       ratsList.push(
         <div key={"rats" + i} id="ratContainer">
